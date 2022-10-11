2789d8f112d3146a58094cddbaef0946be38288f
Roman Samoylin <roman.samoylin@gmail.com>
2022-10-11 20:12:36 +0300

    Task1.1

--- HW2/Task1/1.py
+++ HW2/Task1/1.py
@@ -1,5 +1,24 @@
-#  Напишите программу, которая принимает на вход число N и выдаёт последовательность из N членов.
+# Напишите программу, которая принимает вещественное число и считает сумму его цифр
 
-n=int(input('Введите число: '))
-for i in range (n):
-    print(f'{(-3)**i} ', end=' ')
+def DigitSum (chislo_list):
+    sum = 0
+    for i in range(len(chislo_list)):
+        while chislo_list[i]>0:
+            sum+=chislo_list[i]%10
+            chislo_list[i]=chislo_list[i]//10
+    return sum
+
+def DigitSumInt (chislo):
+    sum = 0
+    while chislo>0:
+        sum+=chislo%10
+        chislo=chislo//10
+    return sum
+
+user_inp = input('Введите вещественное: ')
+if ('.' or ',' in user_inp):
+    user_inp=user_inp.replace(',','.')
+    chislo = list(map(int, user_inp.split('.')))
+    print(DigitSum(chislo))
+else:
+    print(DigitSum(DigitSumInt(int(user_inp))))
